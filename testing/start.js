b2a643164ca7f9ad0e25b8ded537539059133434
flim0012 <flim0012@student.monash.edu>
2022-08-12 17:31:44 +0000

    Upload New File

---
+++ testing/start.js
@@ -0,0 +1,16 @@
+"use strict"
+
+
+/**
+ * startingPage function
+ * runs after user clicks the "plan vacation" button in the home page
+ * set the staeting location variable into an empty string
+ */
+ 
+function startingPage() {
+    window.location = "mainpage.html";
+}
+
+function proceedToAdd(){
+    window.location = "addtask.html";
+}
