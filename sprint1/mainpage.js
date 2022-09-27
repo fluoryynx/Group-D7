398bbcdff5dd7b9972580e4a2d5e2f0565b3db0e
esoh0005 <esoh0005@student.monash.edu>
2022-09-28 03:08:29 +0800

    Reverting a mistake of git merge orgin

--- sprint1/mainpage.js
+++ sprint1/mainpage.js
@@ -4,14 +4,14 @@ if (checkLSData(TASK_LIST_KEY)) {
 	savedTasks.fromData(taskListData);
 }
 
-if (checkLSData(SPRINT_LIST_KEY)) {
-	savedSprints.fromData(sprintListData);
-}
+// console.log(TASK_LIST_KEY)
+// console.log(checkLSData(TASK_LIST_KEY))
+// console.log(retrieveLSData(TASK_LIST_KEY))
+// console.log(data)
 
 let arr = savedTasks._allTask;
 let filteredTag="";
-let taskInfo="";
-let taskIndex="";
+
 
 /**
  * pageLoad function
@@ -21,15 +21,14 @@ let taskIndex="";
  function pageLoad() {
 	let taskList = document.getElementById("taskList");
 
+
 	if (filteredTag != "") {
 		arr = searchTaskWithTag(filteredTag);
 
 	}
 	// console.log(arr);
 	let taskListInnerHTML = "";
-	console.log(arr);
 	for (let i in arr) {
-		//temp="Task name: " + arr[i][0]._taskName , "\n" , "tag: " , (arr[i][0]._taskTag) , "\n description: ",arr[i][0]._taskDescription,"\n" , "assignee(s): " , arr[i][0]._taskAssignee , "\n" , "story point: " , arr[i][0]._storyPoint, "\n Priority: ", arr[i][0]._taskPriority;
 		taskListInnerHTML += `  
 		<div class="mdl-cell mdl-cell--4-col" >
 								<h5> 
@@ -56,7 +55,6 @@ let taskIndex="";
 									<button class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored" onclick="deleteTask(${i})">  <i class="material-icons">delete</i> </button>
 									&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 									<button onclick="edit(${i})" class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored"> <i class="large material-icons">edit</i> </button>
-									<button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" onclick="addToSprint(${i})"> Add to sprint board</button>
 									</p>
 									</div>
 									<div class="mdl-card__actions mdl-card--border">
@@ -84,6 +82,7 @@ function searchTaskWithTag(tag){
 	return searchedTasks;
 }
 
+
 function filterByTag(){
 	let tagRef = document.getElementById("filterBy");
 	filteredTag = tagRef.options[tagRef.selectedIndex].value;
@@ -99,50 +98,7 @@ function removeFilter(){
 	pageLoad();
 }
 
-//dialog that pops out when the add to sprint button is clicked
-let dialog = document.getElementById("sprintDialog");
-if (!dialog.showModal) {
-  dialogPolyfill.registerDialog(dialog);
-}
-
-//show dialog for user to input sprint details
-function addToSprint(index){
-taskIndex=index;
-dialog.showModal();
-}
-
-//cancel add to sprint
-function cancel() {
-	dialog.close();
-  }
-
-function confirmAddSprint(){
-let sprintBoard=document.getElementById("sprintBoard");
-let sprintName=document.getElementById("sprintName");
-let startDate=document.getElementById("sprintStartDate");
-let endDate=document.getElementById("sprintEndDate");
-let temp= savedTasks._allTask[taskIndex][0];
-
-if(sprintBoard==""||sprintName==""|| startDate=="" || endDate==""){
-    alert('name and dates must be filled in')
-    return
-  }
-
-  sprint._sprintBoard=sprintBoard.value;
-  sprint._sprintName=sprintName.value;
-  sprint._sprintStartingDate=startDate.value;
-  sprint._sprintEndingDate=endDate.value;
-  sprint._sprintInfo="Task name: " + temp._taskName + " Tag: " + temp._taskTag + " Description: "+ temp._taskDescription + "\n" + "assignee(s): " + temp._taskAssignee , "\n" + "story point: " + temp._storyPoint+ "\n Priority: "+ temp._taskPriority;
-
-  if (confirm(`Clicking this will add this task into the sprint list. Are you sure you want to continue?`)) {
-    savedSprints._allSprint.push(sprint);
-    updateLSData(SPRINT_LIST_KEY, savedSprints);
-    dialog.close();
-  }
-  window.location = "sprintlist.html";
-}
 
 window.onload = function () {
     pageLoad();
 };
-
--- sprint1/sprintlist.js
+++ sprint1/sprintlist.js
@@ -1,80 +1,39 @@
 
 // If there is, store it into savedSprints
-
 if (checkLSData(SPRINT_LIST_KEY)) {
 	savedSprints.fromData(sprintListData);
 }
 
-//savedSprints=retrieveLSData(SPRINT_LIST_KEY);
-
 let arr = savedSprints._allSprint;
-let filteredTag2 = "";
-let sprintIndex = "";
-let content = "";
 
-let boards = [];
-
-function removeDuplicates(arr) {
-	return arr.filter((item,
-		index) => arr.indexOf(item) === index);
-}
-
-
-console.log(boards);
 /**
  * pageLoad function
  * runs when the list.html page is load
  * use to show list of planned vacation on the page in mdl cards sorted by date
  */
-function pageLoad() {
-
-	for (let i in arr) {
-		boards.push(arr[i]._sprintBoard);
-	}
-
+ function pageLoad() {
 	let sprintList = document.getElementById("sprintList");
-
-	let sprintBoardSelections = document.getElementById("sprint_boards_filter");
-
-	//dropdown list to select sprint board
-	let option = "<option>"
-	"</option>";
-	for (let i in removeDuplicates(boards)) {
-		option += `<option>${boards[i]}</option>}`;
-	}
-	sprintBoardSelections.innerHTML = option;
-
-	if (filteredTag2 != "") {
-		console.log(filteredTag2);
-		arr = searchSprintWithBoard(filteredTag2);
-	}
 
 	let sprintListInnerHTML = "";
 	for (let i in arr) {
-		console.log(arr[i]._sprintInfo);
 		sprintListInnerHTML += `  
 		<div class="mdl-cell mdl-cell--3-col" >
             <h5> 
-            Task ${Number(i) + 1}
+            Sprint ${Number(i) + 1}
             </h5>
 								<div class="mdl-card"  > 
 									<div class="mdl-card__supporting-text"> 
-									<b>Sprint board name:</b> ${arr[i]._sprintBoard}
+                                    <b>Sprint name:</b> ${arr[i][0]._sprintName}
                                     <br><br>
-                                    <b>Sprint name:</b> ${arr[i]._sprintName}
-                                    <br><br>
-									<b>From:</b> ${arr[i]._sprintStartingDate}
+									<b>From:</b> ${arr[i][0]._sprintStartingDate}
 									<br><br>
-									<b>To: </b> ${arr[i]._sprintEndingDate}
+									<b>To: </b> ${arr[i][0]._sprintEndingDate}
 									<br><br>
-									<b> Time remaining: </b>
-									<p id="timer${i}"></p>
-									<b> Status: </b><${arr[i]._sprintStatus}> ${arr[i]._sprintStatus} </${arr[i]._sprintStatus}>
+									<b> Status: </b><${arr[i][0]._sprintStatus}> ${arr[i][0]._sprintStatus} </${arr[i][0]._sprintStatus}>
 									<br><br>
 									<p>
 									<button class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored" onclick="deleteSprint(${i})">  <i class="material-icons">delete</i> </button>
 									&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-									<button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" onclick="viewInfo(${i})"> Task information </button>
 									<button onclick="editSprint(${i})" class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored"> <i class="large material-icons">edit</i> </button>
 									</p>
 									</div>
@@ -82,82 +41,19 @@ function pageLoad() {
 									</div> 
 								</div> 
 							</div>	`
-		countdown(arr[i]._sprintEndingDate, i);
 	}
 	sprintList.innerHTML = sprintListInnerHTML;
 }
 
 
-function deleteSprint(index) {
+function deleteSprint(index){
 	deleteIndex = index
 	savedSprints._allSprint.splice(index, 1);
-	updateLSData(SPRINT_LIST_KEY, savedSprints);
+	updateLSData(SPRINT_LIST_KEY,savedSprints);
 	pageLoad();
 }
 
-
-let dialog2 = document.getElementById("sprintInfoDialog");
-if (!dialog2.showModal) {
-	dialogPolyfill.registerDialog(dialog2);
-}
-
-function viewInfo(index) {
-	sprintIndex = index;
-//	content=info
-	dialog2.showModal();
-//	let infoPlaceholder=document.getElementById("productBacklogInfo");
-	//infoPlaceholder.value=info;
-}
-
-// close dialog
-function cancel2() {
-	dialog2.close();
-}
-
-function countdown(endDate, index) {
-	let countDownDate = new Date(endDate + " 23:59:59").getTime();
-
-	// Update the count down every 1 second
-	let x = setInterval(function () {
-
-		// Get today's date and time
-		let now = new Date().getTime();
-
-		// Find the distance between now and the count down date
-		let distance = countDownDate - now;
-
-		// Time calculations for days, hours, minutes and seconds
-		let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-		let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-		let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-		let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-		document.getElementById("timer" + index).innerHTML = days + "d " + hours + "h "
-			+ minutes + "m " + seconds + "s ";
-
-		if (distance < 0) {
-			clearInterval(x);
-			document.getElementById("timer" + index).innerHTML = "overdue";
-		}
-	}, 1000);
-}
-
-function filterBySprintBoards() {
-	let tagRef = document.getElementById("sprint_boards_filter");
-	filteredTag2 = tagRef.options[tagRef.selectedIndex].value;
-	pageLoad()
-}
-
-function searchSprintWithBoard(sprint) {
-	let searchedSprints = [];
-	for (let i in savedSprints._allSprint) {
-		if (savedSprints._allSprint[i]._sprintBoard == sprint) {
-			searchedSprints.push(savedSprints._allSprint[i]);
-		}
-	}
-	return searchedSprints;
-}
 
 window.onload = function () {
-	pageLoad();
+    pageLoad();
 };
