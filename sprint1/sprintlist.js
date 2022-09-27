d9a95f6bb2c300012831b147528272734ed0aec2
FLUORYYNX,_LIM <flim0012@student.monash.edu>
2022-09-27 21:33:41 +0800

    timer

--- sprint1/sprintlist.js
+++ sprint1/sprintlist.js
@@ -80,11 +80,9 @@ let x = setInterval(function() {
   let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   let seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
-  // Output the result in an element with id="demo"
   document.getElementById("timer"+index).innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
     
-  // If the count down is over, write some text 
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("timer"+index).innerHTML = "overdue";
