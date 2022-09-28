5eea524a7507efbdf33c07e54ceeafa991f6cfc1
Kennedy Tan <ktan0087@student.monash.edu>
2022-09-29 03:19:50 +0800

    U10T1: FIxed some bugs on display

--- sprint1/sprintlist.js
+++ sprint1/sprintlist.js
@@ -51,11 +51,49 @@ function pageLoad() {
 					</div>
 				</div>
 			</div>  `
-		countdown(arr[i][0]._sprintEndingDate, i);
+		//countdown(arr[i][0]._sprintEndingDate, i);
+	}
+	else{
+		sprintListInnerHTML += ` 
+		<div class="mdl-cell mdl-cell--4-col" >
+			<sprint>Sprint ${Number(i) + 1}</sprint>
+			<br><br>
+				<div class="mdl-card">
+					<div class="mdl-card__supporting-text">
+						<div class="row">
+							<div class="column">
+								<sprintname>Sprint </sprintname><sprintnametext>${arr[i]._sprintName}</sprintnametext>
+								<br><br>
+								<fromdate>From:</fromdate>
+								<br>
+								<fromdatetext>${arr[i]._sprintStartingDate}</fromdatetext>
+								<img src="img/date.png" class="dateimgsprint">
+							</div>
+							<div class="column2">
+								<br><br>
+								<todate>To: </todate><br><todatetext>${arr[i]._sprintEndingDate}</todatetext>
+								<img src="img/date.png" class="dateimgsprint">
+							</div>
+							</b><${arr[i]._sprintStatus}> ${arr[i]._sprintStatus} </${arr[i]._sprintStatus}>
+							<br><br>
+							<p>
+							<button class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored" onclick="deleteSprint(${i})">  <i class="material-icons">delete</i> </button>
+							&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+							<button onclick="editSprint(${i})" class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored"> <i class="large material-icons">edit</i> </button>
+							&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+							<button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" onclick="viewBoard(${i})">Sprint board</button>
+							</p>
+						</div>
+					</div>
+					<div class="mdl-card__actions mdl-card--border">
+					</div>
+				</div>
+			</div>  `
+		//countdown(arr[i][0]._sprintEndingDate, i);
 	}
 	sprintList.innerHTML = sprintListInnerHTML;
  }
- 
+}
  
  function deleteSprint(index){
 	deleteIndex = index
@@ -73,32 +111,32 @@ function viewBoard(index) {
 }
 
 
-function countdown(endDate, index) {
-	let countDownDate = new Date(endDate + " 23:59:59").getTime();
+// function countdown(endDate, index) {
+// 	let countDownDate = new Date(endDate + " 23:59:59").getTime();
 
-	// Update the count down every 1 second
-	let x = setInterval(function () {
+// 	// Update the count down every 1 second
+// 	let x = setInterval(function () {
 
-		// Get today's date and time
-		let now = new Date().getTime();
+// 		// Get today's date and time
+// 		let now = new Date().getTime();
 
-		// Find the distance between now and the count down date
-		let distance = countDownDate - now;
+// 		// Find the distance between now and the count down date
+// 		let distance = countDownDate - now;
 
-		// Time calculations for days, hours, minutes and seconds
-		let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-		let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-		let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-		let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+// 		// Time calculations for days, hours, minutes and seconds
+// 		let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+// 		let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+// 		let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+// 		let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-		document.getElementById("timer" + index).innerHTML = days + "d " + hours + "h "
-			+ minutes + "m " + seconds + "s ";
+// 		document.getElementById("timer" + index).innerHTML = days + "d " + hours + "h "
+// 			+ minutes + "m " + seconds + "s ";
 
-		if (distance < 0) {
-			clearInterval(x);
-			document.getElementById("timer" + index).innerHTML = "overdue";
-		}
-	}, 1000);
-}
+// 		if (distance < 0) {
+// 			clearInterval(x);
+// 			document.getElementById("timer" + index).innerHTML = "overdue";
+// 		}
+// 	}, 1000);
+// }
 
 pageLoad();
